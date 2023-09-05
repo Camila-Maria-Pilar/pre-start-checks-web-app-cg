8f21dcaf6056174f92bb17802b1df2176d9352ed
Camila Galleguillos <camila.m.galleguillos@gmail.com>
2023-09-05 15:55:34 +0800

    config connection change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.set('strictQuery', false);
+//mongoose.set('strictQuery', false);
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pre-check-start-app',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pre-check-start-app',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
